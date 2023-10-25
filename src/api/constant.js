30e330cd32cc73952636843ed6950a3ad4f6111a
ApexcupDeveloper <57320815+ApexcupDeveloper@users.noreply.github.com>
2023-10-26 05:00:45 +0800

    Update constant.js

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,13 +1,13 @@
-export const API_BASE_URL = 'https://dev.stambz.org/api/v6/en';
+export const API_BASE_URL = 'https://www.example.com/api/v6/en';
 
-export const ASSET_URL = 'https://dev.stambz.org/assets/uploads/{TYPE}/thumbs/';
+export const ASSET_URL = 'https://www.example.com/assets/uploads/{TYPE}/thumbs/';
 
-export const CAROUSEL_URL = 'https://dev.stambz.org/assets/uploads/vendors/';
+export const CAROUSEL_URL = 'https://www.example.com/assets/uploads/vendors/';
 
-export const NOTI_URL = 'https://dev.stambz.org/assets/uploads/notifications/';
+export const NOTI_URL = 'https://www.example.com/assets/uploads/notifications/';
 
-export const PROFILE_URL = 'https://dev.stambz.org/assets/uploads/app_users/';
+export const PROFILE_URL = 'https://www.example.com/assets/uploads/app_users/';
 
-export const TERMS_URL = 'https://dev.stambz.org/terms-of-services';
+export const TERMS_URL = 'https://www.example.com/terms-of-services';
 
-export const API_GOOGLE_MAP = 'AIzaSyCQmCmGXvkqZKe9ragXfMcbUoGn6cvWJs0';
+export const API_GOOGLE_MAP = 'API_GOOGLE_MAP';
